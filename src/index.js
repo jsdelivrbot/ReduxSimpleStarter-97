0a0c2204a8500f090162ebcddf24c7aa2c840c9c
jiachen <380188469@qq.com>
2017-08-29 14:44:39 +1000

    add Youtube API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-// Creat a new component. This component should produce some HTML
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyD71tdbPviplyO4OLqFJmg84_2CR7lzuvM';
-const App = () => {
-  return <div>Hi!</div>;
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos:[] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
-
-// Take this component's generated HTML and put it on the page(in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
